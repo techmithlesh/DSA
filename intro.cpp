123bfca91e9f53373ac440e4f375d757519bb5eb
Mithlesh Kumar <mithleshwebdeveloper@gmail.com>
2023-06-22 09:44:50 +0530

    DSA-Learning: Personal journey in mastering Data Structures & Algorithms, featuring daily progress, code implementations, explanations, and notes.

--- intro.cpp
+++ intro.cpp
@@ -4,4 +4,40 @@ using namespace std;
 int main(){
     cout<<"Hello World!" <<endl;
     cout<<"Starting DSA Journey";
+
+    // Q1. I/P-> a,b if a>b -> Answer is 'A' Else b>a -> 'B'
+
+    char a, b;
+    cout<<"Enter the Character";
+    cin>>a>>b;
+
+    if (a >b )
+    {
+        cout<<"A";
+    }else{
+        cout<<"B";
+    }
+    
+    // Q2. 'a' -> 'z' => "Lowercase" 'A' -> 'Z' => "Uppercase" '0' -> '9' => "Number"
+
+    char ch;
+    cout<<"Enter the Character" <<endl;
+    cin>>ch;
+
+    if (islower(ch))
+    {
+        cout<<"It is lowercase";
+    }
+    else if (isupper(ch))
+    {
+         cout<<"It is Uppercase";
+
+    }
+    else if(isdigit(ch))
+    {
+       cout<<"It is a Numeric";
+    }else{
+        cout<<"Other Character";
+    }
+    
 }
---
+++ loop.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // Q1. Find sum of all Natural Number
+
+   /* int i=1, n;
+    int sum =0;
+    cout<<"Enter Natural Number N " <<endl;
+    cin>>n;
+
+    while (i<=n)
+    {
+       sum = sum + i; 
+       i = i + 1;
+    }
+
+    cout<<"The Value of Sum of Natural Number is: " <<sum;
+
+    */
+
+    //    Q2. Find sum of all even no 
+
+    int i=1, n;
+    // int sum = 0;
+    // cout<<"Enter the Value of N" <<endl;
+    // cin>>n;
+
+    // while (i<=n)
+    // {
+    //     if (i % 2 == 0) {
+    //         sum += i;
+    //     }
+    //     i++;
+    // }
+    // cout<<"The sum of all even Number is: " <<sum;
+
+    
+    
+}
